a44ae02dda9ec54674249f5050a52bdd2a1dcf9a
tsikot39 <corpusjohnbenedict@gmail.com>
2024-06-05 20:50:24 -0600

    javascript-essentials

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -7,6 +7,11 @@ let userMessage;
 let userType = "subscriber";
 let userCategory;
 
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated
+  ? "Authenticated"
+  : "Not authenticated";
+
 if (userRole === "admin") {
   accessLevel = "Full access granted";
 } else if (userRole === "manager") {
@@ -42,3 +47,4 @@ switch (userType) {
 console.log("Access Level:", accessLevel);
 console.log("User Message:", userMessage);
 console.log("User Category:", userCategory);
+console.log("Authentication Status:", authenticationStatus);
